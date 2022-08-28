5a21790cb59f0ebcc89bacc5dff9c259766297a6
fmfontana <aniciomanlio01@gmail.com>
2022-08-28 18:07:52 -0300

    Update seed.ts

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -8,6 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 	try {
 		await db.dbConnect();
+		productModel.deleteMany();
 		productModel.insertMany(products);
 		await db.dbDisconnect();
 		res.status(200).json("Data loaded");
