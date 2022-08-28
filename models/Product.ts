c4916ac8e54edb5661a05a7b50c657e5ea922f24
fmfontana <aniciomanlio01@gmail.com>
2022-08-28 11:28:37 -0300

    models

---
+++ models/Product.ts
@@ -0,0 +1,46 @@
+import mongoose, { Model, Schema } from "mongoose";
+import { Product } from "./../interfaces/Product";
+
+const productSchema = new Schema(
+	{
+		description: { type: String, required: true },
+		images: [{ type: String }],
+		stock: { type: Number, required: true, default: 0 },
+		price: { type: Number, required: true, default: 0 },
+		sizes: [
+			{
+				type: String,
+				enum: {
+					values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+					message: "{VALUE} is an invalid size",
+				},
+			},
+		],
+		slug: { type: String, required: true, unique: true },
+		tags: [{ type: String }],
+		title: { type: String, required: true },
+		type: {
+			type: String,
+			enum: {
+				values: ["shirts", "pants", "hoodies", "hats"],
+				message: "{VALUE} is an invalid type",
+			},
+		},
+		gender: {
+			type: String,
+			enum: {
+				values: ["men", "women", "kid", "unisex"],
+				message: "{VALUE} is an invalid genre",
+			},
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+productSchema.index({ title: "text", tags: "text" });
+
+const Product: Model<Product> = mongoose.models.Product || mongoose.model("Product", productSchema);
+
+export default Product;
