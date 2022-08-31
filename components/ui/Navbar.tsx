a9e7a8da08cca368f0f5bbccde97a89d88854e04
fmfontana <aniciomanlio01@gmail.com>
2022-08-31 09:48:54 -0300

    Update Navbar.tsx

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 import { AppBar, Badge, Box, Button, IconButton, Link, Toolbar, Typography } from "@mui/material";
 import SearchOutlined from "@mui/icons-material/SearchOutlined";
@@ -6,6 +7,8 @@ import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 
 export const Navbar = () => {
+	const router = useRouter();
+
 	return (
 		<AppBar>
 			<Toolbar>
@@ -22,22 +25,22 @@ export const Navbar = () => {
 				<Box sx={{ display: { xs: "none", sm: "block" } }}>
 					<NextLink href="/" passHref>
 						<Link>
-							<Button color="primary">All</Button>
+							<Button color={router.asPath == "/" ? "secondary" : "primary"}>All</Button>
 						</Link>
 					</NextLink>
 					<NextLink href="/category/men" passHref>
 						<Link>
-							<Button color="primary">Mens</Button>
+							<Button color={router.asPath == "/category/men" ? "secondary" : "primary"}>Mens</Button>
 						</Link>
 					</NextLink>
 					<NextLink href="/category/women" passHref>
 						<Link>
-							<Button color="primary">Womens</Button>
+							<Button color={router.asPath == "/category/women" ? "secondary" : "primary"}>Womens</Button>
 						</Link>
 					</NextLink>
 					<NextLink href="/category/kid" passHref>
 						<Link color="primary">
-							<Button color="primary">Kids</Button>
+							<Button color={router.asPath == "/category/kid" ? "secondary" : "primary"}>Kids</Button>
 						</Link>
 					</NextLink>
 				</Box>
